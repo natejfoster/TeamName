fa627e327a222525afa09b2b820d5e935eade4b7
natejfoster <nathan.96@comcast.net>
2017-05-23 10:09:38 -0700

    added an example page

--- src/example.js
+++ src/example.js
@@ -3,8 +3,11 @@ $(function() {
 
     d3.tsv("data/data.tsv", function(error, data) {
         var formatDate = d3.timeParse("%Y");
+        console.log(data);
         var myChart = LineChart()
-            .xValue(function(d) { return formatDate(d.date);})
+            .xValue(function(d) {
+              console.log(d.date);
+              return formatDate(d.date);})
             .yValue(function(d) { return +d["Wall"]; }) // Currently takes in only final one
             .width(1000)
             .height(500)
@@ -13,9 +16,9 @@ $(function() {
             .xAxisTitle("Time")
             .yAxisTitle("Word Usage (per Million)")
             .title("Language in Motion (Mock Data)")
-            
+
         var chartWrapper = d3.select("#vis")
-                        .datum(data) 
-                        .call(myChart); 
+                        .datum(data)
+                        .call(myChart);
     })
 })
