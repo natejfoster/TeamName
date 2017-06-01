c23131f390d746869367933f4df746ed205cee8d
andrewk7 <andrewk7@uw.edu>
2017-06-01 15:14:20 -0700

    Tech story

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ var data = [{
     value: 34
 }];
 
+
 // Function to lock and unlock the position of the vis based on the passed in
 // start and stop values.
 $.fn.lockLocation = function(start, stop) {
@@ -31,57 +32,76 @@ $.fn.lockLocation = function(start, stop) {
 }
 
 $(function() {
-    // Initialize charting tool
-    var myChart = BarChart().xVar('name')
-        .yVar('value')
-        .xAxisLabel('Bar')
-        .yAxisLabel('Arbitrary Value')
-        .width(500)
-        .height(250);
+     /* ********************************** Data prep  ********************************** */
+    $('#TechnologyButton').click(changeChartFilter);
 
-    // Build chart
-    var chart = d3.select('#vis')
-        .datum(data)
-        .call(myChart);
-
-    $('#vis').lockLocation(720, 3400);
-
-    var update = function(index) {
-        switch (index) {
-            case 0:
-                var fillColor = 'blue';
-                break;
-            case 1:
-                var fillColor = 'red';
-                break;
-            case 2:
-                var fillColor = 'orange';
-                break;
-            case 3:
-                var fillColor = 'black';
-                break;
-            case 4:
-                var fillColor = 'green';
-                break;
-            case 5:
-                var fillColor = 'purple';
-                break;
-            default:
-                var fillColor = 'black';
-                break;
-        }
-        myChart.fillColor(fillColor);
-        chart.datum(data).call(myChart);
+    function changeChartFilter(event, element){
+            console.log(event);
+            console.log("HI");
+            d3.csv('data/ngramClassification.csv', function(error, data2) {
+            console.log(data);
+            data = data2
+            console.log(data);
+            console.log(data[0]['Type']);
+            var nested_data = d3.nest().key(function(d) { return d.Type; })
+            .entries(data);
+            console.log(nested_data);
+            return nested_data;
+        });
     };
-    // Create scroller
-    var scroll = scroller()
-        .container(d3.select('#graphic'));
+       
+    
+        // Initialize charting tool
+        var myChart = BarChart().xVar('name')
+            .yVar('value')
+            .xAxisLabel('Bar')
+            .yAxisLabel('Arbitrary Value')
+            .width(500)
+            .height(250);
 
-    // Select all steps
-    scroll(d3.selectAll('.step'));
+        // Build chart
+        var chart = d3.select('#vis')
+            .datum(data)
+            .call(myChart);
 
-    // Add the update function to all steps
-    scroll.on('active', function(index) {
-        update(index);
-    })
+        $('#vis').lockLocation(720, 3400);
+
+        var update = function(index) {
+            switch (index) {
+                case 0:
+                    var fillColor = 'blue';
+                    break;
+                case 1:
+                    var fillColor = 'red';
+                    break;
+                case 2:
+                    var fillColor = 'orange';
+                    break;
+                case 3:
+                    var fillColor = 'black';
+                    break;
+                case 4:
+                    var fillColor = 'green';
+                    break;
+                case 5:
+                    var fillColor = 'purple';
+                    break;
+                default:
+                    var fillColor = 'black';
+                    break;
+            }
+            myChart.fillColor(fillColor);
+            chart.datum(data).call(myChart);
+        };
+        // Create scroller
+        var scroll = scroller()
+            .container(d3.select('#graphic'));
+
+        // Select all steps
+        scroll(d3.selectAll('.step'));
+
+        // Add the update function to all steps
+        scroll.on('active', function(index) {
+            update(index);
+        })
 });
